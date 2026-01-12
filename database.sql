1ef166b1c40d0a0861de871af5911ecc2a61f073
Loïc LAFFINEUR <laffineur.loic@gmail.com>
2026-01-12 17:31:28 +0100

    update database.sql

--- database.sql
+++ database.sql
@@ -1,4 +1,10 @@
-CREATE DATABASE pizza;
+-- Ouvrir terminal Ctrl + shift + ù
+-- psql -U postgre -h localhost
+-- mot de passe : ?6CCW38dzcr9
+
+CREATE DATABASE pizza; 
+
+\c pizza;
 
 CREATE TABLE pizzas (
     pizza_id INTEGER PRIMARY KEY,
@@ -15,5 +21,4 @@ CREATE TABLE pizzas (
     pizza_name TEXT
 );
 
-\copy pizzas(pizza_id, order_id, pizza_name_id, quantity, order_date, order_time, unit_price, total_price, pizza_size, pizza_category, pizza_ingredients, pizza_name) FROM 'C:/Users/laffi/OneDrive/Desktop/Projets/BI_Pizza/data/pizza_sales.csv'
-DELIMITER ',' CSV HEADER;
+\copy pizzas (pizza_id, order_id, pizza_name_id, quantity, order_date, order_time, unit_price, total_price, pizza_size, pizza_category, pizza_ingredients, pizza_name) FROM 'C:\Users\laffi\Desktop\Projet Perso Info\Python\DS\Dashboard_Pizza\data\pizza_sales.csv' DELIMITER ',' CSV HEADER;  
